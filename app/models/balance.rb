036e438b8ac9a624d11c40015bc056594075c98e
Sergey Gaychuk <sergey.gaychuk@gmail.com>
2011-01-20 16:00:42 +0200

    model: refactoring state and balance

--- app/models/balance.rb
+++ app/models/balance.rb
@@ -1,3 +1,12 @@
-class Balance < State
-  validates :value, :uniqueness => true
+require "state_action"
+
+class Balance < ActiveRecord::Base
+  validates :amount, :value, :start, :side, :deal, :presence => true
+  validates_inclusion_of :side, :in => ["passive", "active"] #[1, 0]
+  validates_uniqueness_of :start, :scope => :deal_id
+  belongs_to :deal
+  
+  after_initialize :do_init
+
+  include StateAction
 end
--- app/models/state.rb
+++ app/models/state.rb
@@ -1,67 +1,11 @@
+require "state_action"
+
 class State < ActiveRecord::Base
   validates :amount, :start, :side, :deal, :presence => true
   validates_inclusion_of :side, :in => ["passive", "active"] #[1, 0]
   belongs_to :deal
   
   after_initialize :do_init
-  
-  def resource
-    return nil if self.deal.nil?
-    return self.deal.take if self.side == "passive"
-    self.deal.give
-  end
 
-  def fact=(aFact)
-    return false if self.deal.nil?
-    return false if aFact.nil?
-    self.start = aFact.day if init_from_fact(aFact)
-    self.start_changed?
-  end
-
-  def amount?
-    self.amount.accounting_zero?
-  end
-  
-  protected
-  def do_init
-    self.side ||= "active"
-    self.amount ||= 0.0
-  end
-  
-  def init_from_fact(aFact)
-    return false if aFact.nil?
-    fact_side =
-      if self.deal == aFact.from
-        "passive"
-      else
-        "active"
-      end
-    
-    if self.side == fact_side
-      self.amount -= aFact.amount
-    else
-      self.amount += aFact.amount * self.rate
-    end
-    
-    if self.amount.accounting_negative?
-      self.side =
-        if self.side == "passive"
-          "active"
-        else
-          "passive"
-        end
-      self.amount *= -1 * self.rate
-    end
-    self.amount = self.amount.accounting_norm
-    true
-  end
-
-  def rate
-    return 0.0 if self.deal.nil?
-    if self.side == "passive"
-      self.deal.rate
-    else
-      1.0 / self.deal.rate
-    end
-  end
+  include StateAction
 end
