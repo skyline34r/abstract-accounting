dab74145a2a3aa3ae83bbdb974c9ad013d8e28a3
Alexey Shepelev <Al.Shepelev@gmail.com>
2011-02-14 16:26:16 +0200

    app: change resource index action on work with jqgrid

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -3,9 +3,31 @@ require 'resource'
 class ResourcesController < ApplicationController
 
   def index
+    @columns = ['tag', 'type', 'id']
     @asset = Asset.select('id, tag, "asset" AS type')
     @money = Money.select('id, alpha_code AS tag, "money" AS type')
     @resources = @money + @asset
+    @resources = @resources.sort do |x,y|
+      if params[:sidx] == 'tag'
+        if params[:sord] == 'asc'
+          x.tag <=> y.tag
+        else
+          y.tag <=> x.tag
+        end
+      else
+        if params[:sord] == 'asc'
+          x.type <=> y.type
+        else
+          y.type <=> x.type
+        end
+      end
+    end
+    @resources = @resources.paginate(
+      :page     => params[:page],
+      :per_page => params[:rows])
+    if request.xhr?
+      render :json => json_for_jqgrid(@resources, @columns)
+    end
   end
 
   def new_asset
