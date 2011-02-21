ada85cdd91f64acdca1c11df95b8ad5acfab9036
Alexey Shepelev <Al.Shepelev@gmail.com>
2011-02-21 17:12:50 +0200

    app: change deal index action on work with jqgrid

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,7 +1,17 @@
 class DealsController < ApplicationController
 
   def index
-    @deals = Deal.all
+    @columns = ['tag', 'entity_tag', 'id', 'rate', 'entity_id', 'give_id',
+                'give_type', 'take_id', 'take_type']
+    @deals = Deal.joins('INNER JOIN entities ON entities.id = deals.entity_id')
+                 .select('"deals".*, "entities".tag AS entity_tag');
+    @deals = @deals.paginate(
+      :page     => params[:page],
+      :per_page => params[:rows],
+      :order    => order_by_from_params(params))
+    if request.xhr?
+      render :json => json_for_jqgrid(@deals, @columns)
+    end
   end
 
   def new
