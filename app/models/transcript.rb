0424019e3e3a162b8ca7d4f4c30cb23464d2cc92
Sergey Gaychuk <sergey.gaychuk@gmail.com>
2011-03-02 19:14:32 +0200

    model: calculate total_credits in transcript

--- app/models/transcript.rb
+++ app/models/transcript.rb
@@ -6,11 +6,12 @@ class Transcript < Array
     @stop = stop
     @total_debits = 0.0
     @total_debits_value = 0.0
+    @total_credits = 0.0
     load_list
     load_diffs
   end
   attr_reader :deal, :start, :stop, :opening, :closing
-  attr_reader :total_debits, :total_debits_value
+  attr_reader :total_debits, :total_debits_value, :total_credits
 
   private
   def load_list
@@ -20,6 +21,8 @@ class Transcript < Array
         if item.fact.to == @deal
           @total_debits += item.fact.amount
           @total_debits_value += item.value + item.earnings
+        elsif item.fact.from == @deal
+          @total_credits += item.fact.amount
         end
       end
     end
