3294958060ded6c8fd46783a1573cee73434856b
Alexey Shepelev <Al.Shepelev@gmail.com>
2011-03-12 14:01:00 +0200

    app: change balance index action on work with jqgrid

--- app/controllers/balances_controller.rb
+++ app/controllers/balances_controller.rb
@@ -1,7 +1,16 @@
 class BalancesController < ApplicationController
 
   def index
+    session[:res_type] = ''
+    @columns = ['deal.tag', 'deal.entity.tag', 'deal.give.tag', 'amount', 'value']
     @balances = BalanceSheet.new DateTime.now
+    @balances = @balances.paginate(
+      :page     => params[:page],
+      :per_page => params[:rows],
+      :order    => order_by_from_params(params))
+    if request.xhr?
+      render :json => abstract_json_for_jqgrid(@balances, @columns)
+    end
   end
   
 end
