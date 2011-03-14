b5b13063c27341d297ae73bea8f41b480ae814b3
Alexey Shepelev <Al.Shepelev@gmail.com>
2011-03-14 19:34:08 +0200

    app: add in index general ledger action jqgrid data

--- app/controllers/general_ledgers_controller.rb
+++ app/controllers/general_ledgers_controller.rb
@@ -8,7 +8,17 @@ class GeneralLedgersController < ApplicationController
         return alpha_code
       end
     }
+    session[:res_type] = ''
+    @columns = ['fact.day', 'fact.resource.tag', 'fact.amount', 'fact.from.tag',
+                'fact.to.tag', 'fact.amount', 'value', 'earnings']
     @general_ledgers = GeneralLedger.new
+    @general_ledgers = @general_ledgers.paginate(
+      :page     => params[:page],
+      :per_page => params[:rows],
+      :order    => order_by_from_params(params))
+    if request.xhr?
+      render :json => abstract_json_for_jqgrid(@general_ledgers, @columns)
+    end
   end
 
 end
