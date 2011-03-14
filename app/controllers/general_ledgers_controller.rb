eb5a88bcdc2b1acee6b8f6c816b711d811595e77
Alexey Shepelev <Al.Shepelev@gmail.com>
2011-03-14 16:27:46 +0200

    app: add method tag in Money resource from general ledger

--- app/controllers/general_ledgers_controller.rb
+++ app/controllers/general_ledgers_controller.rb
@@ -1,6 +1,13 @@
+require 'resource.rb'
+
 class GeneralLedgersController < ApplicationController
 
   def index
+    Money.class_exec {
+      def tag
+        return alpha_code
+      end
+    }
     @general_ledgers = GeneralLedger.new
   end
 
