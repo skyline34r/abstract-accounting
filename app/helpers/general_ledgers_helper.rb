5416919fbc0d80aab97d022f98e877929d3b5c6a
Alexey Shepelev <Al.Shepelev@gmail.com>
2011-03-14 19:35:30 +0200

    app: create helper with jqgrid data of general ledger

---
+++ app/helpers/general_ledgers_helper.rb
@@ -0,0 +1,86 @@
+module GeneralLedgersHelper
+
+  include JqgridsHelper
+
+  def general_ledgers_jqgrid
+
+    options = {:on_document_ready => true}
+
+    grid = [{
+      :url => '/general_ledgers',
+      :datatype => 'json',
+      :mtype => 'GET',
+      :colNames => ['date', 'resource', 'quantity', 'DC', 'deal', 'price',
+                    'debit', 'credit'],
+      :colModel => [
+        { :name => 'date',     :index => 'fact.day',          :width => 100,
+          :formatter => 'function(cellvalue, options, rowObject) {
+                           return cellvalue.substr(0,10);
+                         }'.to_json_var
+        },
+        { :name => 'resource', :index => 'fact.resource.tag', :width => 100 },
+        { :name => 'quantity', :index => 'fact.amount',       :width => 100 },
+        { :name => 'DC',       :index => 'DC',                :width => 100,
+          :formatter => 'function(cellvalue, options, rowObject) {
+                           if (cellvalue == "debit") return cellvalue;
+                           return "credit";
+                         }'.to_json_var
+        },
+        { :name => 'deal', :index => 'deal',                  :width => 100,
+          :formatter => 'function(cellvalue, options, rowObject) {
+                           if (rowObject.deal) return rowObject.deal;
+                           return rowObject[3];
+                         }'.to_json_var
+        },
+        { :name => 'price', :index => 'price',                :width => 100,
+          :formatter => 'function(cellvalue, options, rowObject) {
+                           if (rowObject[5] == 0) return "0";
+                           if (rowObject.price) return rowObject.price;
+                           return ((rowObject[6] + rowObject[7]) / rowObject[5]).toFixed(2);
+                         }'.to_json_var
+        },
+        { :name => 'debit', :index => 'debit',                :width => 100,
+          :formatter => 'function(cellvalue, options, rowObject) {
+                           if (rowObject.debit) return rowObject.debit;
+                           return "";
+                         }'.to_json_var
+        },
+        { :name => 'credit', :index => 'credit',              :width => 100,
+          :formatter => 'function(cellvalue, options, rowObject) {
+                           if (rowObject.credit) return "";
+                           return ((rowObject[6] + rowObject[7]) / rowObject[5]
+                                   * rowObject[2]).toFixed(2);
+                         }'.to_json_var
+        }
+      ],
+      :pager => '#general_ledger_pager',
+      :rowNum => 10,
+      :rowList => [10, 20, 30],
+      :viewrecords => true,
+      :afterInsertRow => 'function(rowid, rowdata, rowelem)
+      {
+         function getPrice() {
+           if (rowelem[5] == 0) return "0";
+           return (rowelem[6] / rowelem[5]).toFixed(2);
+         }
+         function getDebit() {
+           return (rowelem[2] * rowelem[6] / rowelem[5]).toFixed(2);
+         }
+         if (rowid != "sub") {
+           $("#general_ledgers_list").addRowData("sub", { date: "",
+                                                          resource: "",
+                                                          quantity: "",
+                                                          DC: "debit",
+                                                          deal: rowelem[4],
+                                                          price: getPrice(),
+                                                          debit: getDebit(),
+                                                          credit: "null" });
+         }
+      }'.to_json_var
+    }]
+    
+    jqgrid_api 'general_ledgers_list', grid, options
+
+  end
+
+end
