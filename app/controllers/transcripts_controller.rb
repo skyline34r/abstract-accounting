2cfb8b754b96a47c66634cec0813362f1b1b9eb2
Alexey Shepelev <Al.Shepelev@gmail.com>
2011-03-15 18:52:57 +0200

    app: add in transcript load action jqgrid data

--- app/controllers/transcripts_controller.rb
+++ app/controllers/transcripts_controller.rb
@@ -10,10 +10,19 @@ class TranscriptsController < ApplicationController
   end
 
   def load
+    @columns = ['fact.day', 'fact.from.tag', 'fact.to.tag', 'fact.amount',
+                'value', 'earnings']
     deal = Deal.find(params[:deal_id])
     @transcript = Transcript.new(deal,
                                  DateTime.strptime(params[:start], '%m/%d/%Y'),
                                  DateTime.strptime(params[:stop], '%m/%d/%Y'))
+    @transcript = @transcript.paginate(
+      :page     => params[:page],
+      :per_page => params[:rows],
+      :order    => order_by_from_params(params))
+    if request.xhr?
+      render :json => abstract_json_for_jqgrid(@transcript, @columns)
+    end
   end
 
 end
