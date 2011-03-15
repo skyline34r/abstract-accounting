a47696e542e7b115035a01bd20b602a1cfdfe03d
Alexey Shepelev <Al.Shepelev@gmail.com>
2011-03-15 15:46:16 +0200

    app: create transcript helper with jqgrid data

---
+++ app/helpers/transcripts_helper.rb
@@ -0,0 +1,30 @@
+module TranscriptsHelper
+
+  include JqgridsHelper
+
+  def transcripts_jqgrid
+
+    options = {:on_document_ready => true}
+
+    grid = [{
+      :url => '/transcripts',
+      :datatype => 'json',
+      :mtype => 'GET',
+      :colNames => ['date', 'deal', 'debit', 'credit'],
+      :colModel => [
+        { :name => 'date',   :index => 'date',   :width => 200 },
+        { :name => 'deal',   :index => 'deal',   :width => 400 },
+        { :name => 'debit',  :index => 'debit',  :width => 100 },
+        { :name => 'credit', :index => 'credit', :width => 100 }
+      ],
+      :pager => '#transcripts_pager',
+      :rowNum => 10,
+      :rowList => [10, 20, 30],
+      :viewrecords => true
+    }]
+
+    jqgrid_api 'transcripts_list', grid, options
+
+  end
+
+end
