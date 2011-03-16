bc9d0b14dc459d134783a7248c303ce769dd2061
Alexey Shepelev <Al.Shepelev@gmail.com>
2011-03-16 12:25:26 +0200

    app: add total variables in load transcript action

--- app/controllers/transcripts_controller.rb
+++ app/controllers/transcripts_controller.rb
@@ -16,12 +16,21 @@ class TranscriptsController < ApplicationController
     @transcript = Transcript.new(deal,
                                  DateTime.strptime(params[:start], '%m/%d/%Y'),
                                  DateTime.strptime(params[:stop], '%m/%d/%Y'))
-    @transcript = @transcript.paginate(
-      :page     => params[:page],
-      :per_page => params[:rows],
-      :order    => order_by_from_params(params))
-    if request.xhr?
-      render :json => abstract_json_for_jqgrid(@transcript, @columns, :id_column => 'id')
+    if params[:source] == 'grid'
+      @transcript = @transcript.paginate(
+        :page     => params[:page],
+        :per_page => params[:rows],
+        :order    => order_by_from_params(params))
+      if request.xhr?
+        render :json => abstract_json_for_jqgrid(@transcript, @columns, :id_column => 'id')
+      end
+    else
+      @total_credits = @transcript.total_credits
+      @total_credits_diff = @transcript.total_credits_diff
+      @total_credits_value = @transcript.total_credits_value
+      @total_debits = @transcript.total_debits
+      @total_debits_diff = @transcript.total_debits_diff
+      @total_debits_value = @transcript.total_debits_value
     end
   end
 
