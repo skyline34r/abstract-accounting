e0ced858a604269cb73dd7d2278bb580d0af426b
Alexey Shepelev <Al.Shepelev@gmail.com>
2011-03-18 19:31:21 +0200

    app: change roles index action on work with jqgrid data

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,7 +1,15 @@
 class RolesController < ApplicationController
 
   def index
-    @roles = Role.all
+    session[:res_type] = ''
+    @columns = ['name', 'id']
+    @roles = Role.paginate(
+      :page     => params[:page],
+      :per_page => params[:rows],
+      :order    => order_by_from_params(params))
+    if request.xhr?
+      render :json => abstract_json_for_jqgrid(@roles, @columns)
+    end
   end
 
   def new
