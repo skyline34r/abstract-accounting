d25c2b81505920db68959ff15ded058187c5697b
Alexey Shepelev <Al.Shepelev@gmail.com>
2011-03-19 00:20:54 +0200

    app: add role fixtures

--- test/unit/role_test.rb
+++ test/unit/role_test.rb
@@ -6,7 +6,7 @@ class RoleTest < ActiveSupport::TestCase
     assert !r.save, "Empty role saved"
     r.name = "admin"
     assert r.save, "Role can't be saved"
-    assert_equal 1, Role.all.count
+    assert_equal 3, Role.all.count
 
     assert_equal "admin", Role.where(:id => r.id).first.name,
       "Role name is not equal to 'admin'"
