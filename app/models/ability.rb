a239fb82053e5aa4963b2456e7163be469fae12c
Alexey Shepelev <Al.Shepelev@gmail.com>
2011-03-19 20:34:30 +0200

    app: create dinamic authentication

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,8 +2,14 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    if user.role? :operator
-      can :manage, :all
+    i = 0
+    while i < Role.count
+      if user.role? Role.all[i].name
+        Role.all[i].pages.each_line(',') do |p|
+          can :manage, eval(p.chomp(','))
+        end
+      end
+      i += 1
     end
   end
 end
