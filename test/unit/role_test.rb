2350274856f9c2799497213ed1e0e05db83479aa
Alexey Shepelev <Al.Shepelev@gmail.com>
2011-03-28 13:25:24 +0300

    model: change name role in test

--- test/unit/role_test.rb
+++ test/unit/role_test.rb
@@ -4,18 +4,18 @@ class RoleTest < ActiveSupport::TestCase
   test "role should save" do
     r = Role.new
     assert !r.save, "Empty role saved"
-    r.name = "admin"
+    r.name = "admin_t"
     assert r.save, "Role can't be saved"
     assert_equal 3, Role.all.count
 
-    assert_equal "admin", Role.where(:id => r.id).first.name,
-      "Role name is not equal to 'admin'"
+    assert_equal "admin_t", Role.where(:id => r.id).first.name,
+      "Role name is not equal to 'admin_t'"
   end
 
   test "role should save with permitted pages" do
     r = Role.new
     assert !r.save, "Empty role saved"
-    r.name = "admin"
+    r.name = "admin_t"
     r.pages = "Entity, Deal"
     assert r.save, "Role can't be saved"
     assert_equal 3, Role.all.count
