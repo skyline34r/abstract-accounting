ec8dce1a777abc35dae9dd0c94c5791005f9e10a
Alexey Shepelev <Al.Shepelev@gmail.com>
2011-04-02 17:48:27 +0300

    app: refactoring entity page for implement cross page data post

--- app/helpers/entities_helper.rb
+++ app/helpers/entities_helper.rb
@@ -14,27 +14,27 @@ module EntitiesHelper
       :colModel => [
         { :name => 'tag',  :index => 'tag',   :width => 800 }
       ],
-      :pager => '#data_pager',
+      :pager => '#entity_pager',
       :rowNum => 10,
       :rowList => [10, 20, 30],
       :sortname => 'tag',
       :sortorder => 'asc',
       :viewrecords => true,
-      :onSelectRow => "function(cell)
+      :onSelectRow => 'function(cell)
       {
-        $('#entity_tag').val($('#data_list').getCell(cell, 'tag'));
-        $('#change_entity').removeAttr('disabled');
-        $('#change_entity').parent().parent().attr('action',
-            '/entities/' + cell + '/edit');
-      }".to_json_var,
-      :beforeSelectRow =>	"function()
+        $("#entity_tag").val($("#entity_list").getCell(cell, "tag"));
+        $("#change_entity").removeAttr("disabled");
+        $("#change_entity").parent().parent().attr("action",
+            "/entities/" + cell + "/edit");
+      }'.to_json_var,
+      :beforeSelectRow =>	'function()
       {
-        if (canSelect) return true;
+        if (canSelectEntity) return true;
         return false;
-      }".to_json_var
+      }'.to_json_var
     }]
 
-    jqgrid_api 'data_list', grid, options
+    jqgrid_api 'entity_list', grid, options
 
   end
 
