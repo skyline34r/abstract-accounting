095640d44eff3d6430ef00a115b1be0dcab0e322
Sergey Gaychuk <sergey.gaychuk@gmail.com>
2011-04-25 14:44:23 +0300

    model: add inclusion for state field in task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,16 @@
 class Task < ActiveRecord::Base
+
+  #states
+  Unconfirmed = 1
+  Unassigned = 2
+  InWork = 3
+  Implemented = 4
+  Closed = 5
+  #states
+
   validates :summary, :status, :reporter, :assignee, :presence => true
   validates :summary, :uniqueness => true
+  validates :status, :inclusion => { :in => [Unconfirmed, Unassigned, InWork, Implemented, Closed] }
   belongs_to :reporter, :class_name => 'User'
   belongs_to :assignee, :class_name => 'User'
 end
--- test/unit/task_test.rb
+++ test/unit/task_test.rb
@@ -9,17 +9,18 @@ class TaskTest < ActiveSupport::TestCase
              :username => "test_root")
     assert u.save, "user is not saved"
     assert Task.new(:summary => "Summary",
-      :status => 1, :reporter => u, :assignee => u).valid?,
+      :status => Task::Unconfirmed, :reporter => u, :assignee => u).valid?,
       "Wrong task valid state"
-    assert Task.new(:status => 1, :reporter => u, :assignee => u).invalid?,
+    assert Task.new(:status => Task::Unconfirmed, :reporter => u,
+      :assignee => u).invalid?,
       "Wrong task invalid state"
     assert Task.new(:summary => "Summary", :reporter => u, :assignee => u).invalid?,
       "Wrong task invalid state"
     assert Task.new(:summary => "Summary",
-      :status => 1, :assignee => u).invalid?,
+      :status => Task::Unconfirmed, :assignee => u).invalid?,
       "Wrong task invalid state"
     assert Task.new(:summary => "Summary",
-      :status => 1, :reporter => u).invalid?,
+      :status => Task::Unconfirmed, :reporter => u).invalid?,
       "Wrong task invalid state"
   end
 
@@ -30,10 +31,36 @@ class TaskTest < ActiveSupport::TestCase
              :username => "test_root")
     assert u.save, "user is not saved"
     assert Task.new(:summary => "Summary",
-      :status => 1, :reporter => u, :assignee => u).save,
+      :status => Task::Unconfirmed, :reporter => u, :assignee => u).save,
       "Task is not saved"
     assert Task.new(:summary => "Summary",
-      :status => 1, :reporter => u, :assignee => u).invalid?,
+      :status => Task::Unconfirmed, :reporter => u, :assignee => u).invalid?,
+      "Task is valid"
+  end
+
+  test "state must be in task enumerated states" do
+    u = User.new(:email => "test@mail.com",
+             :password => "sasasasa",
+             :password_confirmation => "sasasasa",
+             :username => "test_root")
+    assert u.save, "user is not saved"
+    assert Task.new(:summary => "Summary",
+      :status => Task::Unconfirmed, :reporter => u, :assignee => u).valid?,
+      "Task is invalid"
+    assert Task.new(:summary => "Summary",
+      :status => Task::Unassigned, :reporter => u, :assignee => u).valid?,
+      "Task is invalid"
+    assert Task.new(:summary => "Summary",
+      :status => Task::InWork, :reporter => u, :assignee => u).valid?,
+      "Task is invalid"
+    assert Task.new(:summary => "Summary",
+      :status => Task::Implemented, :reporter => u, :assignee => u).valid?,
+      "Task is invalid"
+    assert Task.new(:summary => "Summary",
+      :status => Task::Closed, :reporter => u, :assignee => u).valid?,
+      "Task is invalid"
+    assert Task.new(:summary => "Summary",
+      :status => 25, :reporter => u, :assignee => u).invalid?,
       "Task is valid"
   end
 end
