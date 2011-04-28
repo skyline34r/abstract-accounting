2a3d217a2f265dc85ef48a130bf83f0078f6b720
Alexey Shepelev <Al.Shepelev@gmail.com>
2011-04-28 16:35:45 +0300

    app: fixed bugs with create sa account

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,16 +4,16 @@ if Role.where(:name => "admin").count < 1 then
   Role.new(:name => "admin").save
 end
 
+User.where(:entity_id => 0).map do |c|
+  if c.email != APP_CONFIG['email'] then
+    c.delete
+  end
+end
+
 if User.where(:entity_id => 0).count < 1 then
   User.new(:email => APP_CONFIG['email'],
            :password => APP_CONFIG['password'],
            :password_confirmation => APP_CONFIG['password'],
            :entity_id => 0,
            :role_ids => [Role.where(:name => "admin").first.id]).save
-else
-  User.where(:entity_id => 0).map do |c|
-    if c.email != APP_CONFIG['email'] then
-      c.delete
-    end
-  end
 end
