49914d584e5b4dd0f325ac8f29e1648b8c4e6899
Sergey Gaychuk <sergey.gaychuk@gmail.com>
2011-04-28 17:15:44 +0300

    merge with alexeysh/storage

--- db/schema.rb
+++ db/schema.rb
@@ -113,15 +113,6 @@ ActiveRecord::Schema.define(:version => 20110428075540) do
     t.datetime "paid"
   end
 
-  create_table "tasks", :force => true do |t|
-    t.string  "summary"
-    t.integer "status"
-    t.integer "reporter_id"
-    t.integer "assignee_id"
-  end
-
-  add_index "tasks", ["summary"], :name => "index_tasks_on_summary", :unique => true
-
   create_table "txns", :force => true do |t|
     t.integer "fact_id"
     t.float   "value"
@@ -132,20 +123,22 @@ ActiveRecord::Schema.define(:version => 20110428075540) do
   add_index "txns", ["fact_id"], :name => "index_txns_on_fact_id", :unique => true
 
   create_table "users", :force => true do |t|
-    t.string   "email",                              :default => "", :null => false
-    t.string   "encrypted_password",  :limit => 128, :default => "", :null => false
-    t.string   "password_salt",                      :default => "", :null => false
+    t.string   "email",                               :default => "", :null => false
+    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
+    t.string   "reset_password_token"
     t.string   "remember_token"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      :default => 0
+    t.integer  "sign_in_count",                       :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "password_salt"
     t.integer  "entity_id"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
   create_table "waybills", :force => true do |t|
     t.datetime "date"
