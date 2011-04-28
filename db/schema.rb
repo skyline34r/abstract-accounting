b343c00ba56af6e30046e6036930943aacc59b44
Sergey Gaychuk <sergey.gaychuk@gmail.com>
2011-04-28 10:39:30 +0300

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -113,6 +113,15 @@ ActiveRecord::Schema.define(:version => 20110427091749) do
     t.datetime "paid"
   end
 
+  create_table "tasks", :force => true do |t|
+    t.string  "summary"
+    t.integer "status"
+    t.integer "reporter_id"
+    t.integer "assignee_id"
+  end
+
+  add_index "tasks", ["summary"], :name => "index_tasks_on_summary", :unique => true
+
   create_table "txns", :force => true do |t|
     t.integer "fact_id"
     t.float   "value"
@@ -123,21 +132,19 @@ ActiveRecord::Schema.define(:version => 20110427091749) do
   add_index "txns", ["fact_id"], :name => "index_txns_on_fact_id", :unique => true
 
   create_table "users", :force => true do |t|
-    t.string   "email",                               :default => "", :null => false
-    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
-    t.string   "reset_password_token"
+    t.string   "email",                              :default => "", :null => false
+    t.string   "encrypted_password",  :limit => 128, :default => "", :null => false
+    t.string   "password_salt",                      :default => "", :null => false
     t.string   "remember_token"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                       :default => 0
+    t.integer  "sign_in_count",                      :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "password_salt"
     t.integer  "entity_id"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
