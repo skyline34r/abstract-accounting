a6a6c893e04af5f172ced78767686a407b5dbdb3
Sergey Gaychuk <sergey.gaychuk@gmail.com>
2011-04-28 12:47:20 +0300

    model: check valid waybill without vatin

--- test/unit/waybill_test.rb
+++ test/unit/waybill_test.rb
@@ -2,7 +2,10 @@ require 'test_helper'
 
 class WaybillTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  test "the truth" do
+  test "validate waybill" do
     assert Waybill.new.invalid?, "Empty waybill is valid"
+    assert Waybill.new(:date => DateTime.now, :owner => entities(:sergey),
+                      :organization => entities(:abstract)).valid?,
+                      "Wrong waybill without vatin"
   end
 end
