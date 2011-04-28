a7dd3218e9746f74e523b630ad75a54e6dbd8b8d
Sergey Gaychuk <sergey.gaychuk@gmail.com>
2011-04-28 16:20:46 +0300

    model: check vatin

--- app/models/waybill.rb
+++ app/models/waybill.rb
@@ -1,5 +1,49 @@
+
+
+#validate vatin only for Russian Federation
+class WaybillValidator < ActiveModel::Validator
+  def validate(record)
+    record.errors[:vatin] <<
+      "Vatin is invalid. Valid count of numbers is 10 or 12" if
+      record.vatin.length != 10 and record.vatin.length != 12
+    record.errors[:vatin] << "VATIN is not numeric" if
+      record.vatin.match(/\A\d+\Z/) == nil
+    record.errors[:vatin] << "Invalid checksum" if !vatin?(record.vatin)
+  end
+
+  def vatin?(vatin)
+    base_prefix = [3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8]
+    valid = false
+    if vatin.length == 10
+      valid = check_sum vatin, base_prefix[2..10], vatin[-1].to_i
+    elsif vatin.length == 12
+      check_sum(vatin, base_prefix[1..10], vatin[-2].to_i) do
+        valid = check_sum vatin, base_prefix, vatin[-1].to_i
+      end
+    end
+    valid
+  end
+
+  def check_sum(vatin, prefix, sum)
+    res = 0
+    prefix.each_index do |index|
+      res += vatin[index].to_i * prefix[index]
+    end
+    if (res % 11) % 10 == sum
+      if block_given?
+        yield
+      else
+        true
+      end
+    else
+      false
+    end
+  end
+end
+
 class Waybill < ActiveRecord::Base
   validates :date, :owner, :organization, :presence => true
+  validates_with WaybillValidator, :if => "!vatin.nil? && !vatin.empty?"
   belongs_to :owner, :class_name => 'Entity'
   belongs_to :organization, :class_name => 'Entity'
 end
--- test/unit/waybill_test.rb
+++ test/unit/waybill_test.rb
@@ -8,4 +8,38 @@ class WaybillTest < ActiveSupport::TestCase
                       :organization => entities(:abstract)).valid?,
                       "Wrong waybill without vatin"
   end
+
+  test "validate VATIN" do
+    assert Waybill.new(:date => DateTime.now, :owner => entities(:sergey),
+              :organization => entities(:abstract), :vatin => "1234").invalid?,
+              "Waybill short vatin number"
+    assert Waybill.new(:date => DateTime.now, :owner => entities(:sergey),
+              :organization => entities(:abstract),
+              :vatin => "1234567890123").invalid?,
+              "Waybill long vatin number"
+    assert Waybill.new(:date => DateTime.now, :owner => entities(:sergey),
+              :organization => entities(:abstract),
+              :vatin => "7830002293").valid?,
+              "Waybill valid vatin number"
+    assert Waybill.new(:date => DateTime.now, :owner => entities(:sergey),
+              :organization => entities(:abstract),
+              :vatin => "7830002295").invalid?,
+              "Waybill invalid vatin number"
+    assert Waybill.new(:date => DateTime.now, :owner => entities(:sergey),
+              :organization => entities(:abstract),
+              :vatin => "500100732259").valid?,
+              "Waybill valid vatin number"
+    assert Waybill.new(:date => DateTime.now, :owner => entities(:sergey),
+              :organization => entities(:abstract),
+              :vatin => "500100732269").invalid?,
+              "Waybill invalid vatin number"
+    assert Waybill.new(:date => DateTime.now, :owner => entities(:sergey),
+              :organization => entities(:abstract),
+              :vatin => "500100732253").invalid?,
+              "Waybill invalid vatin number"
+    assert Waybill.new(:date => DateTime.now, :owner => entities(:sergey),
+              :organization => entities(:abstract),
+              :vatin => "1234d678901a").invalid?,
+              "Waybill invalid vatin number"
+  end
 end
