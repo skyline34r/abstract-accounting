e2ebe0a5ebdb8fae9587158bc9566177fe73aba3
Alexey Shepelev <Al.Shepelev@gmail.com>
2011-04-29 11:06:35 +0300

    app: create for waybills controller create action

--- app/controllers/waybills_controller.rb
+++ app/controllers/waybills_controller.rb
@@ -2,4 +2,10 @@ class WaybillsController < ApplicationController
   def new
     @waybill = Waybill.new
   end
+
+  def create
+    @waybill = Waybill.new(params[:waybill])
+    @waybill.save
+    render :action => "new"
+  end
 end
--- test/functional/waybills_controller_test.rb
+++ test/functional/waybills_controller_test.rb
@@ -7,4 +7,15 @@ class WaybillsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should create waybills" do
+    assert_difference('Waybill.count') do
+       xml_http_request :post, :create,
+                        :waybill => { :date => DateTime.now,
+                                      :owner => entities(:sergey),
+                                      :organization => entities(:abstract),
+                                      :vatin => '500100732259' }
+    end
+    assert_equal 1, Waybill.where(:vatin =>'500100732259').count,
+      'Waybill not saved'
+  end
 end
