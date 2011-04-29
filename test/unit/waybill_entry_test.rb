6e375023c097adb4c3912fcad27ee48277a7edaf
Sergey Gaychuk <sergey.gaychuk@gmail.com>
2011-04-29 12:45:48 +0300

    model: add waybill association for entries

--- test/unit/waybill_entry_test.rb
+++ test/unit/waybill_entry_test.rb
@@ -22,4 +22,18 @@ class WaybillEntryTest < ActiveSupport::TestCase
                   :organization => entities(:abstract)),
       :unit => "th").invalid?, "Invalid waybill entry"
   end
+
+  test "relations between waybill and waybill_entries" do
+    wb = Waybill.new(:date => DateTime.now, :owner => entities(:sergey),
+                  :organization => entities(:abstract))
+    assert wb.save, "Save waybill with entries"
+    wb.waybill_entries.create(:resource => assets(:sonyvaio),
+      :unit => "th", :amount => 10)
+    wb.waybill_entries.create(:resource => Asset.new(:tag => "wire"),
+      :unit => "m", :amount => 25)
+    assert_equal 2, WaybillEntry.all.length, "Waybill entries count is equal to 2"
+    assert WaybillEntry.new(:waybill => wb, :resource => Asset.new(:tag => "edger"),
+      :unit => "th", :amount => 20).save, "Save waybill entry"
+    assert_equal 3, WaybillEntry.all.length, "Waybill entries count is equal to 3"
+  end
 end
