694ec6c610d5c18aaf30ac17a10dbe61ae6b2fe7
Alexey Shepelev <Al.Shepelev@gmail.com>
2011-04-30 17:50:01 +0300

    app: configured waybill jqgrid

--- app/helpers/waybills_helper.rb
+++ app/helpers/waybills_helper.rb
@@ -8,41 +8,28 @@ module WaybillsHelper
 
     grid = [{
       :datatype => 'local',
-      :colNames => ['resource', 'amount', 'unit'],
+      :colNames => ['resource*', 'amount*', 'unit*'],
       :colModel => [ { :name => 'resource', :index => 'resource', :editable => true,
                        :width => 300 },
                      { :name => 'amount',   :index => 'amount',   :editable => true,
-                       :width => 120 },
+                       :width => 120,       :formatter => 'integer',
+                       :formatoptions => {:defaultValue => ''} },
                      { :name => 'unit',     :index => 'unit',     :editable => true,
                        :width => 80 }],
       :pager => '#waybills_pager',
       :rowNum => 10,
+      :cellEdit => true,
+      :editurl => 'clientArray',
+      :cellsubmit => 'clientArray',
       :rowList => [10, 20, 30],
+      :rownumbers => true,
       :sortname => 'resource',
       :sortorder => 'asc',
       :viewrecords => true,
-      :onSelectRow => 'function(id) {
-        if(editSelId >= 0) {
-          $("#waybills_list").saveRow(editSelId, false, "clientArray");
-          $("#waybills_list").restoreRow(editSelId);
-          editSelId = -1;
-        }
-      }'.to_json_var,
-      :ondblClickRow => 'function(id) {
-        if(editSelId >= 0) {
-          $("#waybills_list").saveRow(editSelId, false, "clientArray");
-          $("#waybills_list").restoreRow(editSelId);
-        }
-        editSelId = id;
-        $("#waybills_list").editRow(id,true);
-      }'.to_json_var,
-      :loadComplete => 'function()
+      :beforeEditCell => 'function(rowid, cellname, value, iRow, iCol)
       {
-        uin = 0;
-        $("#waybills_list").addRowData(uin, { resource: ""
-                                            , amount: ""
-                                            , unit: "" });
-        uin++;
+        editRowId = iRow;
+        editCellId = iCol;
       }'.to_json_var
     }]
 
@@ -58,19 +45,22 @@ module WaybillsHelper
                                             , amount: ""
                                             , unit: "" });
         uin++;
-       }'.to_json_var }]
-    pager_button_edit = [:navButtonAdd, '#waybills_pager', {:caption => 'Edit',
-      :buttonicon => 'ui-icon-pencil', :onClickButton =>
-      'function() {
-       }'.to_json_var }]
+      }'.to_json_var }]
     pager_button_del = [:navButtonAdd, '#waybills_pager', {:caption => 'Del',
       :buttonicon => 'ui-icon-trash', :onClickButton =>
       'function() {
-         $("#waybills_list").delRowData($("#waybills_list").getGridParam("selrow"));
-       }'.to_json_var }]
+        if($("#waybills_list").getGridParam("selrow") != null) {
+          $("#waybills_list").delRowData($("#waybills_list").getGridParam("selrow"));
+          var data = $("#waybills_list").getRowData();
+          $("#waybills_list").clearGridData();
+          for(uin=0; uin<data.length; uin++) {
+            $("#waybills_list").addRowData(uin, data[uin]);
+          }
+        }
+      }'.to_json_var }]
 
-    jqgrid_api 'waybills_list', grid, pager, pager_button_add, pager_button_edit,
-                                pager_button_del, options
+    jqgrid_api 'waybills_list', grid, pager, pager_button_add, pager_button_del,
+                                options
 
   end
 
