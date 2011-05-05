9049d3f909a6a157a37f5cfbebc9903735d09c66
Alexey Shepelev <Al.Shepelev@gmail.com>
2011-05-05 12:20:10 +0300

    app: fixed bug with null entry param

--- app/controllers/waybills_controller.rb
+++ app/controllers/waybills_controller.rb
@@ -11,14 +11,16 @@ class WaybillsController < ApplicationController
   end
 
   def create
-    entry = Array.new(params[:entry_resource].length);
-    for i in 0..params[:entry_resource].length-1
-      entry[i] = WaybillEntry.new(:unit => params[:entry_unit][i],
-                                  :amount => params[:entry_amount][i])
-      entry[i].assign_resource_text(params[:entry_resource][i])
-    end
     @waybill = Waybill.new(params[:waybill])
-    @waybill.waybill_entries = entry
+    if params[:entry_resource] != nil then
+      entry = Array.new(params[:entry_resource].length);
+      for i in 0..params[:entry_resource].length-1
+        entry[i] = WaybillEntry.new(:unit => params[:entry_unit][i],
+                                    :amount => params[:entry_amount][i])
+        entry[i].assign_resource_text(params[:entry_resource][i])
+      end
+      @waybill.waybill_entries = entry
+    end
     if params[:organization_text] != '' then
       @waybill.assign_organization_text(params[:organization_text])
     end
