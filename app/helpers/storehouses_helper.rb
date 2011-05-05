cfd9f7356c9b56bdd99a11e356b6517be68d2795
Alexey Shepelev <Al.Shepelev@gmail.com>
2011-05-05 17:44:13 +0300

    app: add storehouse helper with storehouse jqgrid data

---
+++ app/helpers/storehouses_helper.rb
@@ -0,0 +1,30 @@
+module StorehousesHelper
+
+  include JqgridsHelper
+
+  def storehouse_jqgrid
+
+    options = {:on_document_ready => true}
+
+    grid = [{
+      :url => '/storehouses/view?entity_id=',
+      :datatype => 'json',
+      :mtype => 'GET',
+      :colNames => ['resource', 'amount'],
+      :colModel => [
+        { :name => 'resource',  :index => 'resource',   :width => 400 },
+        { :name => 'amount',  :index => 'amount',   :width => 400 }
+      ],
+      :pager => '#storehouse_pager',
+      :rowNum => 10,
+      :rowList => [10, 20, 30],
+      :sortname => 'resource',
+      :sortorder => 'asc',
+      :viewrecords => true
+    }]
+
+    jqgrid_api 'storehouse_list', grid, options
+
+  end
+
+end
