f4f34d0e1ac3b56946479da09c35621e919b9eaa
Alexey Shepelev <Al.Shepelev@gmail.com>
2011-05-05 17:52:44 +0300

    app: implement set storehouses url jqgrid

--- app/helpers/storehouses_helper.rb
+++ app/helpers/storehouses_helper.rb
@@ -20,7 +20,12 @@ module StorehousesHelper
       :rowList => [10, 20, 30],
       :sortname => 'resource',
       :sortorder => 'asc',
-      :viewrecords => true
+      :viewrecords => true,
+      :beforeRequest => 'function()
+      {
+        $("#storehouse_list").setGridParam({url: "/storehouses/view?entity_id="
+                                                   + getOwnerId()});
+      }'.to_json_var
     }]
 
     jqgrid_api 'storehouse_list', grid, options
