d2de6c3f1434c0a1e101fef660b079876e596db0
Alexey Shepelev <Al.Shepelev@gmail.com>
2011-05-06 13:48:24 +0300

    app: add in storehouse helper jqgrid with realise data

--- app/helpers/storehouses_helper.rb
+++ app/helpers/storehouses_helper.rb
@@ -32,4 +32,49 @@ module StorehousesHelper
 
   end
 
+
+  def storehouse_realise_jqgrid
+
+    options = {:on_document_ready => true}
+
+    grid = [{
+      :url => '/storehouses/view?entity_id=',
+      :datatype => 'json',
+      :mtype => 'GET',
+      :colNames => ['', 'resource', 'amount', 'realise'],
+      :colModel => [
+        { :name => '',  :index => 'check', :width => 14,
+          :formatter => 'function(cellvalue, options, rowObject) {
+                           return "";
+                         }'.to_json_var },
+        { :name => 'resource',  :index => 'resource',   :width => 380,
+          :formatter => 'function(cellvalue, options, rowObject) {
+                           return rowObject[0];
+                         }'.to_json_var },
+        { :name => 'amount',  :index => 'amount',   :width => 200,
+          :formatter => 'function(cellvalue, options, rowObject) {
+                           return rowObject[1];
+                         }'.to_json_var },
+        { :name => 'realise',  :index => 'realise',   :width => 200,
+          :formatter => 'function(cellvalue, options, rowObject) {
+                           return "";
+                         }'.to_json_var }
+      ],
+      :pager => '#storehouse_realise_pager',
+      :rowNum => 10,
+      :rowList => [10, 20, 30],
+      :sortname => 'resource',
+      :sortorder => 'asc',
+      :viewrecords => true,
+      :beforeRequest => 'function()
+      {
+        $("#storehouse_realise_list").setGridParam({url: "/storehouses/view?entity_id="
+                                                          + getOwnerId()});
+      }'.to_json_var
+    }]
+
+    jqgrid_api 'storehouse_realise_list', grid, options
+
+  end
+
 end
