eadf70dd9b190d871227f717d22693e463b7e35a
Sergey Gaychuk <sergey.gaychuk@gmail.com>
2011-05-06 18:04:06 +0300

    model: save entries in storehouse release

--- app/models/storehouse_release.rb
+++ app/models/storehouse_release.rb
@@ -1,15 +1,31 @@
+require "resource"
+
+class StorehouseReleaseEntry
+  attr_reader :resource, :amount
+  def initialize(resource, amount)
+    @resource = resource
+    @amount = amount
+  end
+end
+
+class StorehouseReleaseValidator < ActiveModel::Validator
+  def validate(record)
+    record.errors[:resources] = "must be not empty" if record.resources.empty?
+  end
+end
 
 class StorehouseRelease < ActiveRecord::Base
-  attr_accessor :owner, :to
-  validates :created, :presence => true
-  validates :owner, :to, :presence => true, :if => "self.state == UNKNOWN"
-  belongs_to :deal
 
   #States
   INWORK = 1
   CANCELED = 2
   APPLIED = 3
   UNKNOWN = 0
+
+  attr_accessor :owner, :to
+  validates :created, :owner, :to, :state, :presence => true
+  validates_with StorehouseReleaseValidator
+  belongs_to :deal
 
   after_initialize :sv_after_initialize
   before_save :sv_before_save
@@ -30,10 +46,11 @@ class StorehouseRelease < ActiveRecord::Base
   end
 
   def add_resource(resource, amount)
+    @entries << StorehouseReleaseEntry.new(resource, amount)
   end
 
-  def entries
-    Array.new
+  def resources
+    @entries
   end
 
   def cancel
@@ -55,6 +72,7 @@ class StorehouseRelease < ActiveRecord::Base
   private
   def sv_after_initialize
     self.state = UNKNOWN if self.id.nil?
+    @entries = Array.new
   end
 
   def sv_before_save
--- test/unit/storehouse_release_test.rb
+++ test/unit/storehouse_release_test.rb
@@ -6,10 +6,12 @@ class StorehouseReleaseTest < ActiveSupport::TestCase
     assert StorehouseRelease.new.invalid?, "Invalid storehaouse release"
     assert StorehouseRelease.new(:created => DateTime.now).invalid?,
       "StorehouseRelease with created field is invalid"
-    assert StorehouseRelease.new(:created => DateTime.now, :owner => Entity.new(:tag => "Test1Entity"),
-      :to => Entity.new(:tag => "Test2Entity")).valid?, "StorehouseRelease is valid"
-    assert StorehouseRelease.new(:created => DateTime.now, :owner => Entity.new(:tag => "Test1Entity"),
-      :to => Entity.new(:tag => "Test2Entity")).save, "StorehouseRelease is not saved"
+    sr = StorehouseRelease.new(:created => DateTime.now, :owner => Entity.new(:tag => "Test1Entity"),
+      :to => Entity.new(:tag => "Test2Entity"))
+    assert sr.invalid?, "StorehouseRelease is invalid"
+    sr.add_resource(Asset.new(:tag => "Resource1"), 2)
+    assert sr.valid?, "StorehouseRelease is valid"
+    assert sr.save, "StorehouseRelease is not saved"
 
     assert_equal 1, StorehouseRelease.all.count, "StorehouseRelease count is not equal to 1"
     assert_equal StorehouseRelease::INWORK, StorehouseRelease.first.state, "State is not equal to inwork"
@@ -18,6 +20,7 @@ class StorehouseReleaseTest < ActiveSupport::TestCase
   test "to as text" do
     sh = StorehouseRelease.new :created => DateTime.now, :owner => Entity.new(:tag => "Test1Entity")
     sh.to = Entity.new(:tag => "HelloWorld1")
+    sh.add_resource(Asset.new(:tag => "Resource1"), 2)
     assert sh.to.instance_of?(Entity), "To field is not entity"
     assert sh.valid?, "Release is not valid"
 
@@ -35,18 +38,41 @@ class StorehouseReleaseTest < ActiveSupport::TestCase
   end
 
   test "cancel" do
-    assert StorehouseRelease.new(:created => DateTime.now, :owner => Entity.new(:tag => "Test1Entity"),
-      :to => Entity.new(:tag => "Test2Entity")).save, "StorehouseRelease is not saved"
+    sr = StorehouseRelease.new(:created => DateTime.now, :owner => Entity.new(:tag => "Test1Entity"),
+      :to => Entity.new(:tag => "Test2Entity"))
+    sr.add_resource(Asset.new(:tag => "Resource1"), 2)
+    assert sr.save, "StorehouseRelease is not saved"
     
-    assert StorehouseRelease.first.cancel, "StorehouseRelease is not canceled"
+    assert sr.cancel, "StorehouseRelease is not canceled"
     assert_equal StorehouseRelease::CANCELED, StorehouseRelease.first.state, "State is not equal to canceled"
   end
 
   test "apply" do
-    assert StorehouseRelease.new(:created => DateTime.now, :owner => Entity.new(:tag => "Test1Entity"),
-      :to => Entity.new(:tag => "Test2Entity")).save, "StorehouseRelease is not saved"
+    sr = StorehouseRelease.new(:created => DateTime.now, :owner => Entity.new(:tag => "Test1Entity"),
+      :to => Entity.new(:tag => "Test2Entity"))
+    sr.add_resource(Asset.new(:tag => "Resource1"), 2)
+    assert sr.save, "StorehouseRelease is not saved"
 
-    assert StorehouseRelease.first.apply, "StorehouseRelease is not applied"
+    assert sr.apply, "StorehouseRelease is not applied"
     assert_equal StorehouseRelease::APPLIED, StorehouseRelease.first.state, "State is not equal to applied"
+  end
+
+  test "entries" do
+    sr = StorehouseRelease.new(:created => DateTime.now, :owner => Entity.new(:tag => "Test1Entity"),
+      :to => Entity.new(:tag => "Test2Entity"))
+    a = Asset.new(:tag => "hello")
+    sr.add_resource(a, 28)
+
+    assert_equal 1, sr.resources.length, "Resources count is not equal to 1"
+    assert sr.resources[0].instance_of?(StorehouseReleaseEntry), "Unknown entry instance"
+    assert_equal a, sr.resources[0].resource, "Wrong resource"
+    assert_equal 28, sr.resources[0].amount, "Wrong amount"
+    a = Asset.new(:tag => "hello2")
+    sr.add_resource(a, 39)
+
+    assert_equal 2, sr.resources.length, "Resources count is not equal to 2"
+    assert sr.resources[1].instance_of?(StorehouseReleaseEntry), "Unknown entry instance"
+    assert_equal a, sr.resources[1].resource, "Wrong resource"
+    assert_equal 39, sr.resources[1].amount, "Wrong amount"
   end
 end
