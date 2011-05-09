62219ff5ac7ba8fa5de86ea5fabc08f774641e45
Alexey Shepelev <Al.Shepelev@gmail.com>
2011-05-09 12:04:48 +0300

    app: implement select jqgrid event in places page

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -19,7 +19,14 @@ module PlacesHelper
       :rowList => [10, 20, 30],
       :sortname => 'tag',
       :sortorder => 'asc',
-      :viewrecords => true
+      :viewrecords => true,
+      :onSelectRow => 'function(cell)
+      {
+        $("#place_tag").val($("#place_list").getCell(cell, "tag"));
+        $("#change_place").removeAttr("disabled");
+        $("#change_place").parent().parent().attr("action",
+            "/places/" + cell + "/edit");
+      }'.to_json_var
     }]
 
     jqgrid_api 'place_list', grid, options
