ba9f48d9a872f87818f8fed58997a08e649df6ae
Alexey Shepelev <Al.Shepelev@gmail.com>
2011-06-04 13:13:44 +0300

    app: implement filter in quotes page

--- app/helpers/quotes_helper.rb
+++ app/helpers/quotes_helper.rb
@@ -12,7 +12,7 @@ module QuotesHelper
       :mtype => 'GET',
       :colNames => ['resource', 'day', 'rate'],
       :colModel => [
-        { :name => 'resource', :index => 'money.alpha_code', :width => 400 },
+        { :name => 'resource', :index => 'resource', :width => 400 },
         { :name => 'day',      :index => 'day',              :width => 200,
           :formatter => 'function(cellvalue, options, rowObject) {
                            return cellvalue.substr(0,10);
@@ -23,9 +23,12 @@ module QuotesHelper
       :pager => '#quotes_pager',
       :rowNum => 10,
       :rowList => [10, 20, 30],
-      :sortname => 'money.alpha_code',
+      :sortname => 'resource',
       :sortorder => 'asc',
+      :height => "100%",
       :viewrecords => true,
+      :gridview => true,
+      :toppager => true,
       :onSelectRow => 'function(cell)
       {
         $("#quote_res_btn").val($("#quotes_list").getCell(cell, "resource"));
@@ -39,7 +42,27 @@ module QuotesHelper
       }'.to_json_var
     }]
     
-    jqgrid_api 'quotes_list', grid, options
+    pager = [:navGrid, '#quotes_pager', {:refresh => false, :add => false,
+                                         :del=> false, :edit => false,
+                                         :search => false, :view => false, :cloneToTop => true},
+                                        {}, {}, {}]
+
+    button_find_data = {
+      :caption => t('grid.btn_find'),
+      :buttonicon => 'ui-icon-search', :onClickButton => 'function() {
+        if(filter) {
+          $("#quotes_list")[0].clearToolbar();
+          filter = false;
+        } else {
+          filter = true;
+        }
+        $("#quotes_list")[0].toggleToolbar();
+      }'.to_json_var }
+
+    pager_button_find = [:navButtonAdd, '#quotes_pager', button_find_data]
+    pager_button_find1 = [:navButtonAdd, '#quotes_list_toppager_left', button_find_data]
+
+    jqgrid_api 'quotes_list', grid, options, pager, pager_button_find, pager_button_find1
 
   end
 
