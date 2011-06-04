7f877ee2a4b2191e0bbdbf2e31095f1f04f1a09f
Alexey Shepelev <Al.Shepelev@gmail.com>
2011-06-04 15:30:37 +0300

    app: implement filter in balance page

--- app/helpers/balances_helper.rb
+++ app/helpers/balances_helper.rb
@@ -14,15 +14,15 @@ module BalancesHelper
                     t('balance.resource'), t('balance.debit'),
                     t('balance.credit'), t('balance.side')],
       :colModel => [
-        { :name => 'deal',     :index => 'deal.tag',        :width => 240 },
-        { :name => 'entity',   :index => 'deal.entity.tag', :width => 230 },
-        { :name => 'resource', :index => 'deal.give.tag',   :width => 230 },
-        { :name => 'debit',    :index => 'amount',          :width => 50,
+        { :name => 'deal',     :index => 'deal',    :width => 240 },
+        { :name => 'entity',   :index => 'entity',  :width => 230 },
+        { :name => 'resource', :index => 'resource',:width => 230 },
+        { :name => 'debit',    :index => 'amount',  :width => 50, :search => false,
           :formatter => 'function(cellvalue, options, rowObject) {
                            if(rowObject[5] == "active") return "";
                            return cellvalue;
                          }'.to_json_var  },
-        { :name => 'credit',   :index => 'value',           :width => 50,
+        { :name => 'credit',   :index => 'value',   :width => 50, :search => false,
           :formatter => 'function(cellvalue, options, rowObject) {
                            if(rowObject[5] == "passive") return "";
                            if($("#physical").is(":checked"))
@@ -38,10 +38,33 @@ module BalancesHelper
       :rowList => [10, 20, 30],
       :sortname => 'deal',
       :sortorder => 'asc',
-      :viewrecords => true
+      :height => "100%",
+      :viewrecords => true,
+      :gridview => true,
+      :toppager => true
     }]
     
-    jqgrid_api 'data_list', grid, options
+    pager = [:navGrid, '#data_pager', {:refresh => false, :add => false,
+                                       :del=> false, :edit => false,
+                                       :search => false, :view => false, :cloneToTop => true},
+                                      {}, {}, {}]
+
+    button_find_data = {
+      :caption => t('grid.btn_find'),
+      :buttonicon => 'ui-icon-search', :onClickButton => 'function() {
+        if(filter) {
+          $("#data_list")[0].clearToolbar();
+          filter = false;
+        } else {
+          filter = true;
+        }
+        $("#data_list")[0].toggleToolbar();
+      }'.to_json_var }
+
+    pager_button_find = [:navButtonAdd, '#data_pager', button_find_data]
+    pager_button_find1 = [:navButtonAdd, '#data_list_toppager_left', button_find_data]
+
+    jqgrid_api 'data_list', grid, options, pager, pager_button_find, pager_button_find1
 
   end
 
