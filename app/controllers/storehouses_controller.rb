e45ebd69ed80b94f2f02f6e864cb9deaf6f81423
Sergey Gaychuk <sergey.gaychuk@gmail.com>
2011-06-16 17:32:39 +0300

    model: refactoring array intersection

--- app/controllers/storehouses_controller.rb
+++ app/controllers/storehouses_controller.rb
@@ -331,7 +331,7 @@ class StorehousesController < ApplicationController
     end
     storehouse_users = User.find_all_by_place_id(current_user.place.id)
     storehouse_users.map do |r|
-      storehouse_worker = r.id if r.role_ids.any? { |role_id| role_ids.include?(role_id) }
+      storehouse_worker = r.id unless (r.role_ids & role_ids).empty?
     end
 
     created = DateTime.now
