5028e7aa3030af26400a2b838d7c71e6264cdbb2
Alexey Shepelev <Al.Shepelev@gmail.com>
2011-06-25 17:57:00 +0300

    app: change main menu

--- public/javascripts/menu.js
+++ public/javascripts/menu.js
@@ -1,12 +1,37 @@
-function mainmenu(){
-$(" #nav ul ").css({display: "none"}); // Opera Fix
-$(" #nav li").hover(function(){
-		$(this).find('ul:first').css({visibility: "visible",display: "none"}).show(400);
-		},function(){
-		$(this).find('ul:first').css({visibility: "hidden"});
-		});
-}
- 
- $(document).ready(function(){					
-	mainmenu();
+var menuContent = "";
+$(document).click(function(event) {
+	switch(event.target.tagName) {
+		case "PRE":
+			if(($(event.target).parent().parent().attr("id") == "nav") &&
+				($(event.target).html() != menuContent)) {
+				$("#nav ul").css({display: "none"});
+				$("#nav pre").removeClass("active");
+				$(event.target).addClass("active");
+				$(event.target).parent().children("ul").css({display: "block"});
+				$("#sub-menu").css({display: "block"});
+				menuContent = $(event.target).html();
+			} else {
+				$("#nav ul").css({display: "none"});
+				$("#nav pre").removeClass("active");
+				$("#sub-menu").css({display: "none"});
+				menuContent = "";
+			}
+			break;
+		case "A":
+			if($(event.target).parent().parent().parent().parent().attr("id") == "nav") {
+				$(event.target).parent().parent().parent().children("pre").addClass("active");
+				$(event.target).parent().parent().css({display: "block"});
+			} else {
+				$("#nav ul").css({display: "none"});
+				$("#nav pre").removeClass("active");
+				$("#sub-menu").css({display: "none"});
+				menuContent = "";
+			}
+			break;
+		default:
+			$("#nav ul").css({display: "none"});
+			$("#nav pre").removeClass("active");
+			$("#sub-menu").css({display: "none"});
+			menuContent = "";
+	}
 });
