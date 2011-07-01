e9e58f6e0a0dc433e5273238f07a02db9ef3b775
Sergey Gaychuk <sergey.gaychuk@gmail.com>
2011-07-01 14:23:53 +0300

    controller: save entity ids

--- app/controllers/entity_reals_controller.rb
+++ app/controllers/entity_reals_controller.rb
@@ -35,6 +35,7 @@ class EntityRealsController < ApplicationController
 
   def create
     @entity_real = EntityReal.new(params[:entity_real])
+    @entity_real.entity_ids = params[:entities].collect { |id| id.to_i }
     if !@entity_real.save
       render :action => "new"
     end
