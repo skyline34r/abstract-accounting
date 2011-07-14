908087bc73e8a5784b42056a3fc06138c245eb67
Sergey Gaychuk <sergey.gaychuk@gmail.com>
2011-07-14 13:59:53 +0300

    rake: add task for cenvert journal and create versions

--- test/unit/journal_test.rb
+++ test/unit/journal_test.rb
@@ -1,22 +1,65 @@
 require 'test_helper'
 
+module PaperTrail
+  module Model
+    module InstanceMethods
+      def create_initial_pt_version
+        record_create if versions.blank?
+      end
+    end
+  end
+end
+
 class JournalTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  test "must be saved when fact is saved" do
-    assert_equal 0, Journal.all.count, "Wrong journal count"
+  test "check journal convertion to paper trail" do
+    assert_equal 0, Journal.all.count, "Wrong journal records count"
+    assert_equal 0, Version.all.count, "Wrong versions count"
 
+    PaperTrail.enabled = false
+
+    u = User.new(:email => "test@mail.com",
+                 :password => "test_pass",
+                 :password_confirmation => "test_pass",
+                 :entity_id => entities(:sergey).id,
+                 :role_ids => [roles(:operator).id])
+    assert u.save, "User can't be saved"
     User.current = entities(:sergey)
 
-    fact1 = Fact.new :amount => 100000.0,
-      :day => DateTime.civil(2007, 8, 27, 12, 0, 0)
-    fact1.to = deals(:bankaccount)
-    fact1.from = deals(:equityshare2)
-    fact1.resource = fact1.from.take
-    assert fact1.save, "Fact not saved"
+    assert Place.new(:tag => "Some test place").save, "Entity not saved"
+    wb = Waybill.new(:owner => entities(:sergey),
+      :document_id => "128345",
+      :place => Place.find_by_tag("Some test place"),
+      :from => "Some organization",
+      :created => DateTime.civil(2011, 4, 4, 12, 0, 0))
+    wb.add_resource assets(:sonyvaio).tag, "th", 10
+    assert wb.save, "Waybill is not saved"
 
-    assert_equal 1, Journal.all.count, "Wrong journal count"
-    assert_equal fact1, Journal.first.fact, "Wrong journal fact"
-    assert !Journal.first.created_at.nil?, "Wrong journal created at"
-    assert_equal User.current, Journal.first.created_by, "Journal wrong created_by"
+    assert_equal 2, Journal.all.count, "Wrong journal records count"
+    assert_equal 0, Version.all.count, "Wrong versions count"
+    PaperTrail.enabled = true
+
+    Fact.paper_trail_off
+    ActiveRecord::Base.descendants.each do |model|
+      if model.methods.include?(:paper_trail_enabled_for_model)
+        model.all.each do |record|
+          record.create_initial_pt_version
+        end
+      end
+    end
+    Fact.paper_trail_on
+
+    Journal.find_each do |j|
+      j.fact.versions.create :event => 'create',
+                             :whodunnit => User.find_by_entity_id(j.created_by_id).id,
+                             :created_at => j.created_at
+    end
+    Fact.find_each do |f|
+      assert_equal 1, f.versions.count, "Wrong versions count"
+      assert_equal Journal.find_by_fact_id(f.id).created_at, f.versions.first.created_at, "Wrong version created at"
+      assert_equal User.find_by_entity_id(Journal.find_by_fact_id(f.id).created_by_id).id,
+                   f.versions.first.whodunnit, "Wrong user id"
+      assert_equal 'create', f.versions.first.event, "Wrong event"
+    end
   end
 end
