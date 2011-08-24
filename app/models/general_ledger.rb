343393606bb7941b28c362cfdae15b4a85be182b
Alexey Shepelev <Al.Shepelev@gmail.com>
2011-08-24 15:09:52 +0300

    model: optimization of the general ledger

--- app/models/general_ledger.rb
+++ app/models/general_ledger.rb
@@ -1,8 +1,16 @@
 
 class GeneralLedger < Array
   def initialize
-    Fact.all.each do |item|
-      self << (item.txn.nil? ? Txn.new(:fact => item, :value => 0, :earnings => 0) : item.txn)
+    sql = "
+    SELECT facts.id AS fact_id, IFNULL(txns.value, 0.0) AS value,
+           IFNULL(txns.status, NULL) AS status, IFNULL(txns.earnings, 0.0) AS earnings
+    FROM facts
+    LEFT JOIN txns ON txns.fact_id == facts.id"
+
+    ActiveRecord::Base.connection.execute(sql).each do |result|
+      attrs = Hash.new
+      result.each { |key, value| attrs[key.to_sym] = value if key.kind_of?(String) }
+      self << Txn.new(attrs)
     end
   end
 end
