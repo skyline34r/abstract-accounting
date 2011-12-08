eb5ea5ff8ba483bd4235c06be46bfedf7b28ef64
Sergey Gaychuk <sergey.gaychuk@gmail.com>
2011-12-08 13:09:50 +0300

    model: authorize by role permissions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,12 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :manage, :all if user.admin?
+    if user.admin?
+      can :manage, :all
+    else
+      user.credentials.each do |credential|
+        can credential.actions.collect { |item| item.to_sym }, credential.document_type.singularize.constantize
+      end unless user.credentials.empty?
+    end
   end
 end
--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -14,5 +14,31 @@ describe Ability do
   it "should have next behaviour" do
     Ability.new(Factory(:user)).should_not be_able_to(:manage, :all)
     Ability.new(UserAdmin.new).should be_able_to(:manage, :all)
+
+    #check permissions
+    credential = Factory(:credential, :document_type => User.name, :actions => [:create])
+    user = Factory(:user, :credentials => [credential])
+    Ability.new(user).should be_able_to(:create, User)
+    user.credentials << Factory(:credential, :document_type => Credential.name, :actions => [:update])
+    Ability.new(user).should be_able_to(:create, User)
+    Ability.new(user).should be_able_to(:update, Credential)
+    credential.actions << :update
+    Ability.new(user).should be_able_to(:update, Credential)
+    Ability.new(user).should be_able_to(:create, User)
+    Ability.new(user).should be_able_to(:update, User)
+    credential.actions = ["create", :update]
+    Ability.new(user).should be_able_to(:update, Credential)
+    Ability.new(user).should be_able_to(:create, User)
+    Ability.new(user).should be_able_to(:update, User)
+    user.credentials << Factory(:credential, :document_type => Group.name, :actions => [:create])
+    Ability.new(user).should be_able_to(:update, Credential)
+    Ability.new(user).should be_able_to(:create, Group)
+    Ability.new(user).should be_able_to(:create, User)
+    Ability.new(user).should be_able_to(:update, User)
+    credential.actions << :read
+    Ability.new(user).should be_able_to(:update, Credential)
+    Ability.new(user).should be_able_to(:read, User)
+    Ability.new(user).should be_able_to(:create, User)
+    Ability.new(user).should be_able_to(:update, User)
   end
 end
